e93b14e39fbc5dcc2d8c6b13d5322bed43111416
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-04-13 13:22:54 -0400

    corrected total genome size calculation to exclude unplaced contigs

--- 01_scripts/utils/variants_prop.sh
+++ 01_scripts/utils/variants_prop.sh
@@ -12,6 +12,9 @@ RAW_SNPS_VCF="$VCF_DIR/SNPs/SNPs_MAF0.05_FMISS0.5.vcf.gz"
 RAW_INDELS_VCF="$VCF_DIR/indels/indels_MAF0.05_FMISS0.5.vcf.gz"
 
 SV_TABLE="$VCF_DIR/SVs/merged_SUPP2.candidates.table" # was produced in genotype_SVs_SRLR by compare_summarize_plot.sh
+EXCL_CHRS="02_infos/excl_chrs.txt"
+CHRS_BED="02_infos/chrs.bed"
+
 
 #ANGSD_FST_VCF="$ANGSD_FST_DIR/"$(basename -s .vcf.gz $FILT_ANGSD_VCF)".SVsFst_"$POP1"_"$POP2".vcf.gz"
 
@@ -19,6 +22,7 @@ SV_TABLE="$VCF_DIR/SVs/merged_SUPP2.candidates.table" # was produced in genotype
 # LOAD REQUIRED MODULES
 module load bcftools/1.13
 module load R/4.1
+module load bedtools/2.30.0
 
 # 1. Extract CHROM, POS and END for easier handling
 ##bcftools query -f '%CHROM\t%POS\t%END\n' $RAW_SV_VCF > $VCF_DIR/SVs/"$(basename -s .vcf.gz $RAW_SV_VCF)"_CHROM_POS_END.table
@@ -31,7 +35,11 @@ module load R/4.1
 #less $VCF_DIR/SVs/"$(basename -s .vcf.gz $RAW_SV_VCF)"_CHROM_POS_END.table | awk '{print $3-$2}' > 
  
 # 2. Count bases in fasta
-GENOME_BP=$(less $GENOME | grep -E '^[ACTGN]+$' | perl -pe 's/[[:space:]]//g' | wc -c)
+## Remove unplaced scaffolds from fasta
+bedtools getfasta -fi $GENOME -bed $CHRS_BED -fullHeader > 03_genome/genome_no_unplaced.fasta
+
+## Count bases
+GENOME_BP=$(less 03_genome/genome_no_unplaced.fasta | grep -E '^[AaCcTtGgNn]+$' | perl -pe 's/[[:space:]]//g' | wc -c)
 
 # 3. Compute number of bp covered by each variant type and proportion of genome covered
 #Rscript 01_scripts/utils/count_bases.R $SV_TABLE $VCF_DIR/SNPs/"$(basename -s .vcf.gz $RAW_SNPS_VCF)"_CHROM_POS_END.table $VCF_DIR/indels/"$(basename -s .vcf.gz $RAW_INDELS_VCF)"_CHROM_POS_END.table $GENOME_BP
