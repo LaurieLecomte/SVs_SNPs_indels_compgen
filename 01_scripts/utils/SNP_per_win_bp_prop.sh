3637f78fee5644dff84d365541c33dc9109a9477
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-06-02 17:26:13 -0400

    scripts for per-window proportion of bp

---
+++ 01_scripts/utils/SNP_per_win_bp_prop.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Compute bp covered by SNPs for a given window size, specified at positional arg 1
+# 02.8_SNPs_rda.sh is a prequisite to this script, which uses the raw 012 matrix and CHROM_POS_END_ID files produced when doing RDA
+
+# manitou
+# srun -p small -c 1 -J SNP_per_win_bp_prop -o log/SNP_per_win_bp_prop_%j.log /bin/sh 01_scripts/utils/SNP_per_win_bp_prop.sh 100000 &
+
+# valeria
+# srun -p ibis_small -c 1 -J SNP_per_win_bp_prop -o log/SNP_per_win_bp_prop_%j.log /bin/sh 01_scripts/utils/SNP_per_win_bp_prop.sh 100000 &
+
+# VARIABLES
+GENOME="03_genome/genome.fasta"
+
+RAW_VCF_DIR="04_vcf/SNPs"
+RAW_SNPS_VCF="$RAW_VCF_DIR/SNPs_MAF0.05_FMISS0.5.vcf.gz"
+
+ANGSD_INPUT_DIR="05_angsd_inputs/SNPs"
+ANGSD_STATS_DIR="06_angsd_stats/SNPs"
+ANGSD_BYPOP_DIR="07_angsd_bypop/SNPs"
+ANGSD_FST_DIR="08_angsd_fst/SNPs"
+
+PCA_DIR="09_pca/SNPs"
+RDA_DIR="10_rda/SNPs"
+#ANNOT_DIR="11_annotation/SNPs"
+GO_DIR="12_go/SNPs"
+
+ID_SEX_POP="02_infos/ID_Sex_Pop_updated.txt"
+
+SNPS_VCF_ANGSD=$RAW_SNPS_VCF
+
+N_IND="$(less $ID_SEX_POP | wc -l)"
+
+
+MIN_MAF=0.05
+MAX_MAF=0.95
+
+POPS_FILE="02_infos/pops.txt"
+
+FILT_ANGSD_VCF="$ANGSD_STATS_DIR/"$(basename -s .vcf.gz $SNPS_VCF_ANGSD)".maf"$MIN_MAF".vcf.gz"
+
+POP1='RO'
+POP2='PU'
+
+#ANGSD_FST_VCF="$ANGSD_FST_DIR/"$(basename -s .vcf.gz $FILT_ANGSD_VCF)".SNPsFst_"$POP1"_"$POP2".vcf.gz" # VCF formatted for angsd, with added Fst values from previous script
+RAW_FST_VCF="$ANGSD_FST_DIR/"$(basename -s .vcf.gz $RAW_SNPS_VCF)".SNPsFst_"$POP1"_"$POP2".vcf.gz" # input VCF, with added Fst values from previous script
+
+GENOME_ANNOT="03_genome/annotation/genome_annotation_table_simplified_1.5.tsv"
+ANNOT_TABLE="03_genome/annotation/"$(basename -s .tsv $GENOME_ANNOT)".table"
+
+WIN_SIZE=$1
+
+CHR_BED="02_infos/chrs.bed"
+
+DENSITY_DIR="density/SNPs"
+
+
+# LOAD REQUIRED MODULES
+module load vcftools/0.1.16
+module load bcftools/1.13
+module load bedtools/2.30.0
+module load bedops/2.4.40
+module load R/4.1
+
+
+# 1. Remove header from CHROM_POS_END_ID file (previously used for RDA and Fisher tests)
+#less $RDA_DIR/"$(basename -s .vcf.gz $RAW_FST_VCF)".CHR_POS_END_ID.table | tail -n+2 > $RDA_DIR/"$(basename -s .vcf.gz $RAW_FST_VCF)".CHR_POS_END_ID.bed
+
+# 2. Split genome by window
+bedops --chop $WIN_SIZE -x $CHR_BED > 02_infos/chrs_win"$WIN_SIZE".bed
+
+# 3. Get number of overlap between fixed size windows and sites 
+bedtools intersect -a 02_infos/chrs_win"$WIN_SIZE".bed -b $RDA_DIR/"$(basename -s .vcf.gz $RAW_FST_VCF)".CHR_POS_END_ID.table -wa -wb > $DENSITY_DIR/SNPs_intersect_win"$WIN_SIZE"bp.txt
+
+# 4. Compute number of bp and prop
+Rscript 01_scripts/utils/per_win_bp.R $DENSITY_DIR/SNPs_intersect_win"$WIN_SIZE"bp.txt 02_infos/OV_to_ssa.txt $WIN_SIZE
---
+++ 01_scripts/utils/per_win_bp.R
@@ -0,0 +1,75 @@
+# Compute proportion of bp covered by variant by window
+
+library(ggplot2)
+
+# 1. Access files in command line, import and format ----------------------
+argv <- commandArgs(T)
+WIN_OVERLAP <- argv[1]
+OV_2_SSA <- argv[2]
+WIN_SIZE <- as.numeric(argv[3])
+
+# Import
+win_table <- read.table(WIN_OVERLAP, col.names = c('WIN_CHROM', 'BIN_START', 'BIN_END', 'CHROM', 'POS', 'END', 'ID'))
+
+## Convert OV chromosomes to ssa notation
+OV_2_sasa <- read.table(OV_2_SSA, col.names = c('CHROM_OV', 'CHROM_SSA'))
+## simplify for plotting
+OV_2_sasa$CHROM_NUM <- sapply(X = OV_2_sasa$CHROM_SSA, FUN = function(x){
+  unlist(strsplit(x, split = 'ssa'))[2]}
+)
+
+OV_2_sasa$CHROM_TYPE <- ifelse(OV_2_sasa$CHROM_SSA %in% c('ssa01-23', paste0('ssa0', seq(2,8))),
+                               yes = 'meta',
+                               no = 'acro')
+win_table <- merge(x = win_table, y = OV_2_sasa, by.x = 'CHROM', by.y = 'CHROM_OV')
+
+
+# 2. Compute variant size and middle position -----------------------------
+win_table$LEN <- ifelse(win_table$POS != win_table$END,
+                        yes = win_table$END - win_table$POS,
+                        no = 1)
+
+win_table$midBIN <- win_table$BIN_START + WIN_SIZE/2
+
+
+
+# 3. Compute number of bp covered by window -------------------------------
+bp_per_win <- aggregate(data = win_table, 
+          LEN ~ CHROM_NUM + midBIN, 
+          FUN = sum)
+
+bp_per_win$prop <- bp_per_win$LEN/WIN_SIZE
+
+# Export
+write.table(bp_per_win, file = paste0(unlist(strsplit(WIN_OVERLAP, split = '.txt')), '_bp_prop.txt'),
+            quote = FALSE, col.names = TRUE, row.names = FALSE, sep = "\t")
+
+# Compute mean
+print(paste('mean number of bp covered by',  WIN_SIZE, 'bp window :', mean(bp_per_win$LEN)))
+print(paste('mean proportion of bp covered by',  WIN_SIZE, 'bp window :', mean(bp_per_win$prop)))
+
+
+# 4. Plot -----------------------------------------------------------------
+prop_plot <- 
+ggplot(data = bp_per_win) +
+  facet_grid(.~CHROM_NUM, scales = 'free_x', space = 'free_x') +
+  geom_point(aes(x = midBIN, y = prop), alpha = 0.6, col = 'darkblue') +
+  theme(panel.spacing = unit(0.8, 'points'),
+        strip.text.x = element_text(size = 5),
+        axis.text.x = element_text(angle = 45, size = 4, hjust = 1),
+        panel.background = element_rect(color = "gray70"),
+        strip.placement = "inside",
+        strip.background = element_rect(colour = 'gray70')
+  ) + 
+  scale_x_continuous(
+    labels = function(x) {
+      round(x/10^8, 1)
+    }
+  ) + 
+  ylim(0, 0.2) + 
+  labs(x = expression(paste('Position (', 10^8, ' bp)' )),
+       y = paste('Base pairs covered by', WIN_SIZE/1000000, 'Mb window'))
+
+ggsave(prop_plot, filename = paste0(unlist(strsplit(WIN_OVERLAP, split = '.txt')), '_bp_prop_plot.jpg'), 
+       width = 10, height = 4)
+saveRDS(prop_plot, file = paste0(unlist(strsplit(WIN_OVERLAP, split = '.txt')), '_bp_prop_plot.rds'))
